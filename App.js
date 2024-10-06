f211b144592ca5612a77a7637dd52d0705305762
gabbygood <gabriellegoylan@gmail.com>
2024-10-06 19:34:44 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
+import { NavigationContainer, crete } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen'
+import { useCallback } from 'react';
+import { EditProfile } from './screens';
+import BottomTabNav from './navigations/BottomTabNav';
 
+
+SplashScreen.preventAutoHideAsync()
+const Stack = createNativeStackNavigator()
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    black: require('./assets/fonts/Inter-Black.ttf'),
+    bold: require('./assets/fonts/Inter-Bold.ttf'),
+    medium: require('./assets/fonts/Inter-Medium.ttf'),
+    regular: require('./assets/fonts/Inter-Regular.ttf'),
+    semibold: require('./assets/fonts/Inter-SemiBold.ttf')
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer onReady={onLayoutRootView}>
+      <Stack.Navigator initialRouteName='BottomTabNavigation'>
+        <Stack.Screen name='BottomTabNavigation' component={BottomTabNav} options={{headerShown: false}}/>
+        <Stack.Screen name="EditProfile" component={EditProfile} options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
