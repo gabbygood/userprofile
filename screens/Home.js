273925364b8a2a68ee385304e40312392b5841eb
gabbygood <85273191+gabbygood@users.noreply.github.com>
2024-10-07 16:18:07 +0800

    added safeview to home

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text style={{ fontFamily: 'bold', fontSize: 24, lineHeight: 22 }}>
+          Home
+        </Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
