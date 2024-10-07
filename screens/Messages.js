8b3edfd22861e3ddb7e387cfecdc9b6db583ec21
gabbygood <85273191+gabbygood@users.noreply.github.com>
2024-10-07 16:20:10 +0800

    added safeview to message fragment

--- screens/Messages.js
+++ screens/Messages.js
@@ -1,11 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Messages() {
   return (
-    <View>
-      <Text>Messages</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text style={{ fontFamily: 'bold', fontSize: 24, lineHeight: 22 }}>
+          Messages
+        </Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
