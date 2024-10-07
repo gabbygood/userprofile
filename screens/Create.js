630d6545fcbf2299a515d19c03ed8e370a336eb8
gabbygood <gabriellegoylan@gmail.com>
2024-10-08 07:34:15 +0800

    update create fragment

--- screens/Create.js
+++ screens/Create.js
@@ -1,11 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Create() {
   return (
-    <View>
-      <Text>Create</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text style={{ marginLeft: 0, fontWeight: 400, fontSize: 16, fontFamily: "bold" }}>
+          Create
+        </Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
