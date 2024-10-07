9e30435203ceabef21f9f8d50f2a0ebc384b52c6
gabbygood <85273191+gabbygood@users.noreply.github.com>
2024-10-07 16:14:15 +0800

    update profile

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,12 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Profile() {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Profile</Text>
+      <View
+        style={{ flex1: 1, flexDirection: 'row', justifyContent: 'center' }}
+      >
+        <Text
+          style={{
+            fontFamily: 'bold',
+            fontSize: 24,
+            lineHeight: 22,
+            paddingHorizontal: 10,
+            alignContent: 'center',
+          }}
+        >
+          Gabrielle Goylan
+        </Text>
+        <Image
+          style={{ borderRadius: 50, shadowOffset: 10 }}
+          source={require('../assets/images/meee2.jpg')}
+        />
       </View>
     </SafeAreaView>
   )
